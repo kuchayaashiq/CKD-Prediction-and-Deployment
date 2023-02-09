78a110f169688ae20156cd1dba6f9602a47255f6
kuchayaashiq <kucchayaashiq@gmail.com>
2023-02-09 10:59:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, url_for, render_template
+from pywebio.platform.flask import webio_view
+from pywebio import STATIC_PATH
+from pywebio.input import *
+from pywebio.output import *
+import argparse
+from pywebio import start_server
+
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+app=Flask(__name__)
+model=pickle.load(open('model.pkl', 'rb'))
+@app.route('/', methods=['GET'])
+def home():
+   return render_template('index.html')
+
+standard_to = StandardScaler()
+@app.route("/predict",methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        WC = float(request.form['wc'])
+        BGR = float(request.form['bgr'])
+        BU = float(request.form['bu'])
+        SC = float(request.form['sc'])
+        PCV = float(request.form['pcv'])
+        AL = float(request.form['al'])
+        HEMO = float(request.form['hemo'])
+        AGE = float(request.form['age'])
+        SU = float(request.form['su'])
+        HTN = float(request.form['htn'])
+
+        prediction=model.predict([[WC,BGR,BU,SC,PCV,AL,HEMO,AGE,SU,HTN]])
+
+        if prediction==1:
+            return render_template('index.html',pred='Patient has Chronic Kidney Disease. ')
+        else:
+            return render_template('index.html',pred='Patient  is safe Patient doesnot have CKD')
+    else:
+        return render_template('index.html')
+
+app.add_url_rule('/tool', 'webio_view', webio_view(predict), methods=['GET','POST', 'OPTIONS'])
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-p", "--port", type=int, default=8080)
+#     args = parser.parse_args()
+#     start_server(predict, port=args.port)
+if __name__=='__main__':
+     app.run()
+    
+# if __name__=='__main__':
+#      app.run(debug=True)
+
+
+
+# app.run(host='localhost', port=80)
